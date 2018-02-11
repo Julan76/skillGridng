b67c59f5dc6000b5058d577d7fafc5042ace7eff
HOUEHOU <julan.houehou@capgemini.com>
2018-02-11 21:52:43 +0100

    feat(save button): create save button. Method saving left

--- src/app/domain-detail/domain-detail.component.ts
+++ src/app/domain-detail/domain-detail.component.ts
@@ -1,6 +1,7 @@
-import {AfterViewInit, Component, Input, OnChanges, OnInit, ViewChild} from '@angular/core';
+import {
+  AfterViewChecked, ChangeDetectorRef, Component, Input, OnChanges, OnInit
+} from '@angular/core';
 import {Domain} from '../dto/domain';
-import { MatRadioChange } from '@angular/material';
 import {Tab} from '../dto/skill';
 
 
@@ -9,63 +10,50 @@ import {Tab} from '../dto/skill';
   templateUrl: './domain-detail.component.html',
   styleUrls: ['./domain-detail.component.css']
 })
-export class DomainDetailComponent implements OnInit,OnChanges {
+export class DomainDetailComponent implements OnInit,OnChanges,AfterViewChecked {
   @Input() domain: Domain;
   skillToModify;
-  tab=[
-    { key: 0, val: true,skill:null },
-    { key: 1, val: false,skill:null },
-    { key: 2, val: false,skill:null },
-    { key: 3, val: false,skill:null }
-  ];
-    ngOnChanges() {
+
+      ngOnChanges() {
       if(this.domain!=null) {
-        for(const i in this.domain.skills ) {
-        var tab1= new Tab({key:0,val:true,skill:this.domain.skills[i].text});
-        var tab2= new Tab({key:1,val:false,skill:this.domain.skills[i].text});
-        var tab3= new Tab({key:2,val:false,skill:this.domain.skills[i].text});
-        var tab4= new Tab({key:3,val:false,skill:this.domain.skills[i].text});
+        if(this.domain.skills[0].tabs==null){
+          for(const i in this.domain.skills ) {
+            var tab1= new Tab({key:0,val:true,skill:this.domain.skills[i].text});
+            var tab2= new Tab({key:1,val:false,skill:this.domain.skills[i].text});
+            var tab3= new Tab({key:2,val:false,skill:this.domain.skills[i].text});
+            var tab4= new Tab({key:3,val:false,skill:this.domain.skills[i].text});
 
-           this.domain.skills[i].tabs=[];
-        this.domain.skills[i].tabs.push(tab1);
-        this.domain.skills[i].tabs.push(tab2);
-        this.domain.skills[i].tabs.push(tab3);
-        this.domain.skills[i].tabs.push(tab4);
+            this.domain.skills[i].tabs=[];
+            this.domain.skills[i].tabs.push(tab1);
+            this.domain.skills[i].tabs.push(tab2);
+            this.domain.skills[i].tabs.push(tab3);
+            this.domain.skills[i].tabs.push(tab4);
 
+          }
         }
       }
 
     }
-  update(skill,index) {
+  updateCheckbox(skill,index) {
     this.skillToModify= this.domain.skills.filter(x => x.text===skill.text);
-
-   // this.skillToModify.tab[index].val = !this.skillToModify.tabs[index].val;
-
-    console.log("la skill ",this.skillToModify);
-
     for(let i =0;i< this.skillToModify[0].tabs.length;i++) {
       if(i===index) {
-        console.log("le tab a modifier ",this.skillToModify[0].tabs[i]);
         if(!this.skillToModify[0].tabs[i].val) {
           this.skillToModify[0].tabs[i].val=true;
         }
-        console.log("after", this.skillToModify[0].tabs[i]);
-       // this.skillToModify[0].tabs[i].val=false;
       }
       else {
         this.skillToModify[0].tabs[i].val=false;
-
       }
-
     }
-    console.log(this.domain.skills);
- //   console.log(this.skillToModify[0]);
   }
 
-  constructor() { }
+  constructor(private cdRef:ChangeDetectorRef) { }
 
   ngOnInit() {
   }
 
-
+  ngAfterViewChecked()  {
+    this.cdRef.detectChanges();
+  }
 }
